4286f05c3260284b4be2357ca1e8fa8c3c7d7aa1
alvincchan <cctimeslipcc@yahoo.com>
2014-04-22 14:24:37 -0700

    d

--- sample-tag.py
+++ sample-tag.py
@@ -18,21 +18,20 @@ password = getpass.getpass()
 filer_name = sys.argv[2]
 filer = NaServer(filer_name,1,6)
 filer.set_admin_user(user_name, password)
-tag = 'None'
-
-#while tag == 'None':
+tag = ""
 while True:
-        execute = NaElement('volume-footprint-get-iter')
+        api = NaElement('volume-footprint-get-iter')
         if tag:
-                execute.child_add_string('tag', tag)
-        validate = filer.invoke_elem(execute)
-        tag = validate.child_get_string('next-tag')
+                api.child_add_string('tag', tag)
+        run = filer.invoke_elem(api)
+        tag = run.child_get_string('next-tag')
 
-        #cmd = filer.invoke_elem(execute)
-        attribinfo = validate.child_get('attributes-list')
+        attribinfo = run.child_get('attributes-list')
         result = attribinfo.children_get()
         for output in result:
                 volname = output.child_get_string('volume')
                 print volname
-        if tag is None:
+        #if tag is None:
+        if not tag:
                 break
+
